d70e1ecaa6461b6f94df3f08fe1204664ad0370f
José Moreno <59598804+juanjosemorenom@users.noreply.github.com>
2025-12-10 04:16:03 -0600

    Asistente del basta con Programación Funcional

---
+++ basta_asist_funcional.py
@@ -0,0 +1,33 @@
+# --- Asistente del Juego Basta (version funcional)
+from random import choice
+from time import sleep
+from os import system
+"""
+        -Obtener letra al azar
+        -Configurar las categorías
+        -Capturar/Ingresar las palabras por categoría
+        -Imprimir palabras ingresadas
+        -Asignar puntos por categoría
+        -Función para controlar una ronda
+        -Función principal, para controlar el juego
+"""
+def obtenerLetra(letras):
+    pass
+
+def configurarCategorias(**kwargs):
+    pass
+
+def capturarPalabras(categorias):
+    pass
+
+def imprimirResumen(categorias, palabras):
+    pass
+
+def asignarPuntos(categorias, puntos):
+    pass
+
+def jugarRonda(categorias, letras, contadprRonda):
+    pass
+
+def juegoBasta():
+    pass
